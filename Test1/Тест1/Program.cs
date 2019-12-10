1e8b11a8b1ac255b0070ad1e3a705234033fdb7b
Иван Еременко <iseremenko@edu.hse.ru>
2019-12-10 19:43:48 +0300

    zzz

---
+++ Test1/Тест1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Тест1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
